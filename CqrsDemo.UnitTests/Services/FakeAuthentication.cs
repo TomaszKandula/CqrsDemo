e845b0b9f1e9eaccf83f1dac9559975bd348bdf9
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-12-07 14:43:26 +0100

    refactor: use fake services in unit tests

---
+++ CqrsDemo.UnitTests/Services/FakeAuthentication.cs
@@ -0,0 +1,17 @@
+﻿using CqrsDemo.Services.Authentication;
+
+namespace CqrsDemo.UnitTests.Services
+{
+    
+    public class FakeAuthentication : Authentication
+    {
+
+        public FakeAuthentication() 
+        {        
+        }
+
+        public override string GetUserId { get => "69078034-7ca9-4c66-893f-2e48c7bdc14a"; }
+
+    }
+
+}
