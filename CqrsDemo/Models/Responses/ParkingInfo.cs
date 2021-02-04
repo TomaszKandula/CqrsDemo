a3588261dd0472407275b3554adafee3ad0d5ddd
Tomasz Kandula <tomasz.kandula@gmail.com>
2021-02-04 13:33:25 +0100

    refactor: remove property name

--- CqrsDemo/Models/Responses/ParkingInfo.cs
+++ CqrsDemo/Models/Responses/ParkingInfo.cs
@@ -1,19 +1,13 @@
-﻿using System.Text.Json.Serialization;
-
-namespace CqrsDemo.Models.Responses
+﻿namespace CqrsDemo.Models.Responses
 {
     public class ParkingInfo
     {
-        [JsonPropertyName("name")]
         public string Name { get; set; }
 
-        [JsonPropertyName("isOpened")]
         public bool IsOpened { get; set; }
 
-        [JsonPropertyName("maximumPlaces")]
         public int MaximumPlaces { get; set; }
 
-        [JsonPropertyName("availablePlaces")]
         public int AvailablePlaces { get; set; }
     }
 }
