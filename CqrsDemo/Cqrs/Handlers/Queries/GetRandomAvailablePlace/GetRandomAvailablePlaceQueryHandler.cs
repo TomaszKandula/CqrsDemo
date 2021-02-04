5890aaa674712a387563b0fcad6ace6285e90ab9
Tomasz Kandula <tomasz.kandula@gmail.com>
2021-02-04 19:47:41 +0100

    fix: re-write handler

--- CqrsDemo/Cqrs/Handlers/Queries/GetRandomAvailablePlace/GetRandomAvailablePlaceQueryHandler.cs
+++ CqrsDemo/Cqrs/Handlers/Queries/GetRandomAvailablePlace/GetRandomAvailablePlaceQueryHandler.cs
@@ -4,6 +4,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CqrsDemo.Database;
+using CqrsDemo.Exceptions;
+using CqrsDemo.Shared.Resources;
 using MediatR;
 
 namespace CqrsDemo.Handlers.Queries.GetRandomAvailablePlace
@@ -20,18 +22,24 @@ namespace CqrsDemo.Handlers.Queries.GetRandomAvailablePlace
         public async Task<GetRandomAvailablePlaceQueryResult> Handle(GetRandomAvailablePlaceQuery Request, CancellationToken CancellationToken) 
         {
             var LRandom = new Random();
+            var LRandomNext = LRandom.Next();
 
-            var LParkingPlace = (await FMainDbContext.ParkingPlaces
+            var LParkingPlace = await FMainDbContext.ParkingPlaces
                 .Include(AParkingPlace => AParkingPlace.ParkingNameNavigation.ParkingPlaces)
                 .Where(AParkingPlace => AParkingPlace.ParkingNameNavigation.IsOpened && AParkingPlace.IsFree)
-                .OrderBy(AParkingPlace => LRandom.Next())
-                .ToListAsync())
-                .FirstOrDefault();
+                .ToListAsync(CancellationToken);
+
+            var LRandomParkingPlace = LParkingPlace.OrderBy(AParkingPlace => LRandom.Next());
+
+            if (!LRandomParkingPlace.Any()) 
+            {
+                throw new BusinessException(nameof(ErrorCodes.ERROR_UNEXPECTED), ErrorCodes.ERROR_UNEXPECTED);
+            }
 
             return new GetRandomAvailablePlaceQueryResult
             {
-                ParkingName = LParkingPlace.ParkingName,
-                Number = LParkingPlace.Number
+                ParkingName = LParkingPlace.First().ParkingName,
+                Number = LParkingPlace.First().Number
             };
         }
     }
